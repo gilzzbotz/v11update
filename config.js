99e0de420ada8449375da35bd28eea35fd01009f
gilzzbotz <104905739+gilzzbotz@users.noreply.github.com>
2022-06-23 06:37:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ let chalk = require('chalk')
 global.owner = [
   ['6281241056195'],
   ['6285730794089'],
-  ['', 'Owner', true]
+  ['6285730794089', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -40,8 +40,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = '@penta_bot'
-  var sticker_author = 'penta'
+  var sticker_name = '@GilzMD'
+  var sticker_author = 'Gilz'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -56,9 +56,9 @@ fs.watchFile(file_exif, () => {
 })
 
 // Sticker WM
-global.packname = sticker_name
-global.author = sticker_author
-global.wm = '© Gempy'
+global.packname = GilzMD
+global.author = Gilz
+global.wm = '©GilzBotz'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
